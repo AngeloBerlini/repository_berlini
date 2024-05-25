50ed2e819567cc6f42cdfc336235b916762c703d
Angelo <angelo.berlini@studenti.isissgobetti.it>
2024-05-25 15:57:22 +0200

    Creato il file definitivo per il progetto di fine anno

--- progetto_finale.py
+++ progetto_finale.py
@@ -3,7 +3,8 @@ import random
 
 # Crea una lista per memorizzare i nomi delle squadre
 campionato = []
-# Chiedi i nomi delle squadre e li aggiunge al campionato
+
+# Chiedi i nomi delle squadre
 for i in range(20):
     nome_squadra = input(f"Inserisci il nome della squadra {i+1}: ")
     campionato.append(nome_squadra)
@@ -14,32 +15,36 @@ def simulazione_campionato() -> dict :
     # Crea un dizionario per memorizzare i punti delle squadre
     punti = {squadra: 0 for squadra in campionato}
 
-    for i in range(38): #20 squadre, ognuna gioca 38 partite
-        giornata = i + 1
-        squadra_casa,squadra_trasferta = random.sample(campionato,2)
+    for giornata in range(1, 39): # 38 giornate
+        print(f"Giornata {giornata}:")
+        for _ in range(10): # 10 partite per giornata
+            squadra_casa, squadra_trasferta = random.sample(campionato, 2)
 
-        # Simula un risultato casuale: vittoria, pareggio o sconfitta
-        risultato = random.choice(["vittoria1", "pareggio", "vittoria2"])
-        
-        # Vittoria squadra 1
-        if risultato == "vittoria1":
-            punti[squadra_casa] += 3
-            print(f"{giornata}^ Giornata: {squadra_casa} ha vinto la partita contro {squadra_trasferta}")
+            # Simula un risultato casuale: vittoria, pareggio o sconfitta
+            risultato = random.choice(["vittoria1", "pareggio", "vittoria2"])
+            
+            # Vittoria squadra 1
+            if risultato == "vittoria1":
+                punti[squadra_casa] += 3
+                print(f"{squadra_casa} ha vinto la partita contro {squadra_trasferta}")
 
-            # Pareggio tra le due squadre   
-        elif risultato == "pareggio":
-            punti[squadra_casa] += 1
-            punti[squadra_trasferta] += 1
-            print(f"{giornata}^ Giornata: {squadra_casa} ha pareggiato con {squadra_trasferta}")
+                # Pareggio tra le due squadre   
+            elif risultato == "pareggio":
+                punti[squadra_casa] += 1
+                punti[squadra_trasferta] += 1
+                print(f"{squadra_casa} ha pareggiato con {squadra_trasferta}")
 
-        # Vittoria squadra 2
-        elif risultato == "vittoria2":
-            punti[squadra_trasferta] += 3
-            print(f"{giornata}^ Giornata: {squadra_trasferta} ha vinto la partita contro {squadra_casa}")
+            # Vittoria squadra 2
+            elif risultato == "vittoria2":
+                punti[squadra_trasferta] += 3
+                print(f"{squadra_trasferta} ha vinto la partita contro {squadra_casa}")
+
+        print() # linea di spazio che separa le giornate
+
     return punti
 
 # Funzione che visualizza la classifica finale 
-def visualizzazione_dell_andamento(punti) -> None:
+def visualizzazione_dell_andamento(punti):
 
     # Ordina le squadre in base ai punti
     classifica_finale = sorted(punti.items(), key=lambda x: x[1], reverse=True)
